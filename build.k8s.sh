9e1aa2337f2b26f0906aa88c89c459e338f9e10c
王少泽 <shaozewang@quarkfinance.com>
2018-03-08 16:15:01 +0800

    add list.k8s

---
+++ build.k8s.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+GCR="k8s.gcr.io/"
+ACCOUNT="wszd/"
+cat list | while read line
+do
+  echo "build ${line} ......................."
+  docker pull ${GCR}${line}
+  docker tag ${GCR}${line} ${ACCOUNT}${line}
+  docker push ${ACCOUNT}${line}
+done
